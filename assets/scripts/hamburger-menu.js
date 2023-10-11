71eef84c8b67690f2eb893a9485ca93f54a72cb8
Agustín Comolli <agustincomolli@gmail.com>
2023-10-11 13:43:09 -0300

    fix(web): Add styles and animations to hamburger-menu

--- assets/scripts/hamburger-menu.js
+++ assets/scripts/hamburger-menu.js
@@ -1,7 +1,6 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerClose = document.querySelector(".hamburger-close");
 const navBar = document.querySelector(".nav-bar");
-
 /**
  * Alternar la visibilidad del menú hamburguesa
  */
@@ -12,5 +11,22 @@ function toggleMenu() {
 };
 
 
-hamburgerMenu.addEventListener("click", toggleMenu);
-hamburgerClose.addEventListener("click", toggleMenu);
+function openMenu() {
+    toggleMenu();
+    // Agrega una clase de animación de apertura.
+    navBar.classList.add("animate__slideInRight");
+}
+
+function closeMenu() {
+    // Agrega una clase de animación de cierre.
+    navBar.classList.add("animate__slideOutRight");
+    // Espera a que termine la animación para cerrar el menú.
+    setTimeout(() => {
+        toggleMenu();
+        // Elimina las clases de animación después de cerrar el menú.
+        navBar.classList.remove("animate__slideInRight", "animate__slideOutRight");
+    }, 650);
+}
+
+hamburgerMenu.addEventListener("click", openMenu);
+hamburgerClose.addEventListener("click", closeMenu);
