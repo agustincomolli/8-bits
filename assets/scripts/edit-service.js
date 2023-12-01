f2a4861055c2e116041c80c5c67676c19f4ce024
Agustín Comolli <agustincomolli@gmail.com>
2023-12-01 12:34:43 -0300

    feat(web): finish admin-services, add & edit services
    feat(web): use apy in services.html
    
    feat(web): add login form

--- assets/scripts/edit-service.js
+++ assets/scripts/edit-service.js
@@ -1,45 +1,98 @@
 /**
- * URL base del servidor backend. Se utiliza para realizar solicitudes HTTP al
- * servidor, especificando la ruta base del API.
+ * URL base para solicitudes al backend que contiene la ruta base del API.
  */
 const URL = "http://localhost:5000";
 
-// Selecciona el formulario en el documento para manejar el evento de envío.
+/**
+ * Elemento del formulario HTML usado para agregar o editar servicios.
+ */
 const addForm = document.querySelector(".service-form");
-// Selecciona el botón de cancelar para manejar el evento de click.
+
+/**
+ * Botón para cancelar la edición y volver a la página de administración de servicios.
+ */
 const btnCancel = document.querySelector("#btn-service-cancel");
 
-
+/**
+ * Obtiene el ID del servicio desde los parámetros de la URL.
+ *
+ * @returns {string} El valor del ID del servicio o null si no está presente.
+ */
 function getServiceId() {
     const queryString = window.location.search;
     const urlSearchParams = new URLSearchParams(queryString);
     return urlSearchParams.get("id");
 };
 
-
+/**
+ * Carga los datos del servicio en el formulario de actualización basándose en el ID proporcionado.
+ * 
+ * @param {string} serviceId El ID del servicio del que se cargarán los datos.
+ */
 async function loadServiceData(serviceId) {
     try {
         const response = await fetch(`${URL}/service/${serviceId}`);
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         };
-        const service = await response.json()
+
+        const service = await response.json();
         document.querySelector("#input-service-name").value = service.name;
         document.querySelector("#input-service-description").value = service.description;
+        
+        // Quita la clase 'hidden' una vez que los datos se han cargado.
+        addForm.classList.remove("hidden");
     } catch (error) {
         console.error('Error al cargar los datos del servicio:', error);
     };
 };
 
+/**
+ * Gestionar la edición de un servicio. Esta función se invoca cuando se envía el formulario.
+ *
+ * @param {Event} event El evento de envío del formulario.
+ */
+async function editService(event) {
+    event.preventDefault();  // Evita que se recargue la página.
+    const serviceId = getServiceId();
+    const dataService = {
+        name: document.querySelector("#input-service-name").value,
+        description: document.querySelector("#input-service-description").value,
+    };
 
-function editService() { };
+    try {
+        const response = await fetch(`${URL}/service/${serviceId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataService)
+        });
 
+        if (!response.ok) {
+            throw new Error("HTTP error! status: ${response.status}");
+        };
+
+        console.log("Editado con exito");
+        window.location.href = "./admin-services.html";
+    } catch (error) {
+        console.error('Error al actualizar el servicio:', error);
+    };
+};
+
+/**
+ * Define el comportamiento cuando el documento HTML ha sido cargado completamente.
+ */
 document.addEventListener("DOMContentLoaded", () => {
-    // Añade un evento de 'submit' al formulario para ejecutar la función 'addService'.
+    // Maneja el envío del formulario invocando la función de edición del servicio.
     addForm.addEventListener("submit", editService);
-    // Añade un evento de 'click' al botón de cancelar para redirigir al usuario.
+    // Redirige al usuario a la lista de servicios al hacer click en cancelar.
     btnCancel.addEventListener("click", () => { window.location.href = './admin-services.html'; });
 
+    // Obtén el ID del servicio de la URL y carga los datos asociados.
     const serviceId = getServiceId();
-    loadServiceData(serviceId);
+    if (serviceId) {
+        loadServiceData(serviceId);
+    }
 });
---
+++ assets/scripts/services.js
@@ -0,0 +1,64 @@
+/**
+ * URL base del servidor backend que indica la ruta para realizar solicitudes al API.
+ */
+const URL = "http://localhost:5000";
+
+/**
+ * Crea una tarjeta HTML con los datos de un servicio.
+ * 
+ * @param {Object} service - Un objeto que contiene los detalles de un servicio, como nombre y descripción.
+ * @returns {string} Una cadena de texto que representa una tarjeta HTML con los datos del servicio.
+ */
+function createCard(service) {
+    let card = `
+    <div class="card-service">
+    <h3 class="service-title">${service.name}</h3>
+    <p class="service-description">${service.description}</p>
+    </div>`;
+    return card;
+};
+
+/**
+ * Obtiene y muestra los servicios del backend.
+ * Realiza una petición HTTP para obtener los datos de los servicios y luego 
+ * los muestra en la página usando tarjetas HTML.
+ */
+async function getServices() {
+    try {
+        // Realiza una solicitud GET para obtener los servicios.
+        const response = await fetch(URL + "/services");
+
+        // Verifica si la solicitud no fue exitosa y lanza un error si es necesario.
+        if (!response.ok) {
+            throw new Error(`¡Error HTTP! estado: ${response.status}`);
+        };
+
+        // Procesa la respuesta convirtiéndola de JSON a un array de objetos de servicio.
+        const data = await response.json();
+
+        // Guarda el HTML de todas las tarjetas de servicio.
+        let allCards = `
+            <div class="service-icon-wrapper">
+                <img src="../assets/images/services.webp" alt="">
+            </div>`;
+
+        // Itera sobre cada servicio, crea su tarjeta y la acumula en la cadena de tarjetas.
+        data.forEach(service => {
+            allCards += createCard(service);
+        });
+
+        // Establece el HTML interno de la lista de tarjetas para mostrar todas las tarjetas 
+        // de servicios en la página.
+        document.querySelector(".services").innerHTML = allCards;
+
+    } catch (error) {
+        // Muestra un mensaje de error en la consola si ocurre algún error al obtener o procesar los datos.
+        console.error('Error al obtener los datos:', error);
+    }
+};
+
+/**
+ * Agrega un escuchador para el evento 'DOMContentLoaded' que llama a 
+ * getServices una vez que el contenido de la página se carga completamente.
+ */
+document.addEventListener("DOMContentLoaded", getServices);
